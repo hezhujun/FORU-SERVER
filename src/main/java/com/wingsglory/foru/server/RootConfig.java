5f4db927d58e369e1d14062e2b5b5ff029e036ff
hezhujun <1214507717@qq.com>
2017-08-31 22:44:39 +0800

    启动定时任务，自动注销过期的任务

--- src/main/java/com/wingsglory/foru/server/RootConfig.java
+++ src/main/java/com/wingsglory/foru/server/RootConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @ComponentScan(basePackages = "com.wingsglory.foru.server",
         excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)})
 @ImportResource(locations = {"classpath:spring-mybatis.xml"})
+@EnableScheduling
 public class RootConfig {
 
 }
