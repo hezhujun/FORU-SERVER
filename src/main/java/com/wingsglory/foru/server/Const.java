2ddad6941f68fcf7f512b004e338016e34f592fb
hezhujun <1214507717@qq.com>
2017-08-31 22:45:29 +0800

    添加极光推送的POM配置

--- src/main/java/com/wingsglory/foru/server/Const.java
+++ src/main/java/com/wingsglory/foru/server/Const.java
@@ -5,4 +5,6 @@ package com.wingsglory.foru.server;
  */
 public class Const {
     public static final String DEFAULT_IMAGE_URL = "https://ps.ssl.qhimg.com/t0123f47c7eae031cbb.jpg";
+    public static final String MASTER_SECRET = "db776214e994e7a2d87b572b";
+    public static final String APP_KEY = "8efb491917ed24458c73528f";
 }
